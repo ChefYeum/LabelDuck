8d8ea2f17e0c91a257cf7f032eacd1f6d3a14a92
chefyeum <chefyeum@gmail.com>
2021-03-07 19:34:44 +0900

    implement button

--- app/lib/tabs/labelTab.dart
+++ app/lib/tabs/labelTab.dart
@@ -3,11 +3,26 @@ import 'package:flutter/material.dart';
 const TextStyle optionStyle =
     TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
 
+var img = Image(
+    image: NetworkImage(
+        'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'));
+
+var button =
+// IconButton(icon: Icon(Icons.star), onPressed: () => print("yee"));
+    ElevatedButton(onPressed: () => print('oink'), child: new Text("Call me"));
+
+var imageStack = Stack(children: [
+  // Positioned(
+  //   child: button,
+  //   bottom: 1,
+  // ),
+  img,
+  button,
+]);
+
 Widget labelTab =
     Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-  Image(
-      image: NetworkImage(
-          'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg')),
+  imageStack,
   Text(
     'Click on the right eye',
     style: optionStyle,
