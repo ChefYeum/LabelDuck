0e02b46b28ff5446b35c7d01ec3de01d52048983
Udai <61244716+udai-arneja@users.noreply.github.com>
2021-03-07 14:58:55 +0000

    Updating Flask file

--- server/server.py
+++ server/server.py
@@ -1,12 +1,21 @@
 from flask import Flask
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
-def getImage():
-    return "gs://bgn-university-hack-rem-1018.appspot.com/catDog.jpeg"
+
+def getImage(filelocation):
+    # using the filelocation the firebase script can be run to update
+    # the firebase storage and database
+    # this function then creates a json which is returned to the front end
+
+    # DATA_OF_IMAGE_ATTRIBUTES = excution on firebasescript.py
+
+    with open("imageattributes.json", "w") as write_file:
+        json.dump(DATA_OF_IMAGE_ATTRIBUTES, write_file)
+    return "imageattributes.json"
 
 
 if __name__ == '__main__':
     app.run()
-
