d6628139b66f3d737f81b667eb02cb56f2426223
chefyeum <chefyeum@gmail.com>
2021-03-08 00:04:55 +0900

    request endpoint for image

--- app/lib/tabs/labelTab.dart
+++ app/lib/tabs/labelTab.dart
@@ -1,14 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 const TextStyle optionStyle =
     TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
 
-var img = Image(
-    image: NetworkImage(
-        'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'));
-
 var button = IconButton(icon: Icon(Icons.star), onPressed: () => print("yee"));
 // ElevatedButton(onPressed: () => print('oink'), child: new Text("Call me"));
+
+const HOST_NAME = 'https://localhost:5000/';
+
+Future<String> fetchImageUrl() async {
+  final response = await http.get(Uri.https(HOST_NAME, 'file'));
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return response.body;
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load album');
+  }
+}
+
+var futureImgUrl = fetchImageUrl();
+
+var img = FutureBuilder<String>(
+  future: futureImgUrl,
+  builder: (context, snapshot) {
+    if (snapshot.connectionState == ConnectionState.done) {
+      return Image.network(
+          snapshot.data ?? 'https://picsum.photos/250?image=5');
+    } else if (snapshot.hasError) {
+      return Image.network(
+          snapshot.data ?? 'https://picsum.photos/250?image=5');
+    }
+
+    // By default, show a loading spinner.
+    return CircularProgressIndicator();
+  },
+);
 
 var imageStack = Stack(children: [
   img,
