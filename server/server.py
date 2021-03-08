f967e56664200ca7330635f72e818237e30ec470
chefyeum <chefyeum@gmail.com>
2021-03-08 12:57:25 +0900

    change template response

--- server/server.py
+++ server/server.py
@@ -5,12 +5,12 @@ app = Flask(__name__)
 
 
 result = {
-    "URL": "https://storage.googleapis.com/bgn-university-hack-rem-1018.appspot.com/processed2.png",
+    "imgUrl": "https://storage.googleapis.com/bgn-university-hack-rem-1019.appspot.com/DSC_1236.JPG",
     "location": [[5,5], [2,2]],
     "objectsList": ["dog", "cat"]
-        }
+}
 
-@app.route('/')
+@app.route('/file')
 def getImage():
     return jsonify(result)
 
